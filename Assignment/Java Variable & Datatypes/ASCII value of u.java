af755714eaf0a61a0e18c05374cfd1c752443135
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-09-08 17:08:45 +0530

    Create ASCII value of u.java

---
+++ Assignment/Java Variable & Datatypes/ASCII value of u.java
@@ -0,0 +1,7 @@
+//Q2 - Print the ASCII value of character ‘U’.
+public class Main {
+    public static void main(String[] args){
+        int x ='U';
+        System.out.println("The ASCII value of U is: "+x);
+    }
+}
