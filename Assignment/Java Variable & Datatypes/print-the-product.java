18dfbcd85874539731a0570f3b9cfe6d4cef11c0
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-09-08 17:03:22 +0530

    Create print-the-product.java

---
+++ Assignment/Java Variable & Datatypes/print-the-product.java
@@ -0,0 +1,8 @@
+//Q1 - Take 2 integer values in two variables x and y and print their product.
+public class Main {
+    public static void main(String[] args) {
+        int x =2;
+        int y =10;
+        System.out.println("Product of x and y is: "+ (x*y));
+    }
+}
