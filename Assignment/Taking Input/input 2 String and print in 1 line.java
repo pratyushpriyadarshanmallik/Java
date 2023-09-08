d4925327b4ae439b6f4a7c2a7a544a29dab010ca
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-09-08 17:41:25 +0530

    Create input 2 String and print in 1 line.java

---
+++ Assignment/Taking Input/input 2 String and print in 1 line.java
@@ -0,0 +1,10 @@
+//Q2 - Input two different string and print them in same line.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String first = input.next();
+        String last = input.next();
+        System.out.println(first+last);
+    }
+}
