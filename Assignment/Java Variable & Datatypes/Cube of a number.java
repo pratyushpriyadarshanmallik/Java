fe236b8564417d6f84ec1ac582c578b4e3d6f17f
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-09-08 17:16:00 +0530

    Create Cube of a number.java

---
+++ Assignment/Java Variable & Datatypes/Cube of a number.java
@@ -0,0 +1,8 @@
+//Q4 - Write a Java program to calculate the cube of a number.
+public class Main {
+    public static void main(String[] args) {
+        int num = 9;
+        int cube = num*num*num;
+        System.out.println("Cube of "+num +" is: "+cube);
+    }
+}
