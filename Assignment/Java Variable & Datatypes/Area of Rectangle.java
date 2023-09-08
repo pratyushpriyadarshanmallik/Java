82656616ed12174db734d1da8d2d37f89092146d
pratyushpriyadarshanmallik <93968652+pratyushpriyadarshanmallik@users.noreply.github.com>
2023-09-08 17:12:38 +0530

    Create Area of Rectangle.java

---
+++ Assignment/Java Variable & Datatypes/Area of Rectangle.java
@@ -0,0 +1,9 @@
+//Q3 - Write a Java program to take the length and breadth of a rectangle and print its area.
+public class Main {
+    public static void main(String[] args){
+        int length = 10;
+        int breadth = 6;
+        int area = (length*breadth);
+        System.out.println("Area of Rectangle is: "+area);
+    }
+}
